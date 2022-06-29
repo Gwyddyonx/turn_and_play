87b0afb0d8fc00521c1c5088c17e78577ee32d4f
Camilo Lopez <jkmlopez23@gmail.com>
2022-06-28 22:14:00 -0500

    revisión de proyecto para juego final

--- src/com/turn_play/concentrese/Parejas.java
+++ src/com/turn_play/concentrese/Parejas.java
@@ -1,45 +1,48 @@
-package com.castaneda.concentrese;
+package com.turn_play.concentrese;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Parejas extends JFrame implements ActionListener {
-    private Tablero t;
+    private Tablero tablero;
     private JButton[][] botones;
     private ImageIcon[] imagenes;
-    private static int sw;
-    private static int a, b, ii, jj;
+    private static int sw;// auxiliares
+    private static int a, b, ii, jj;// auxiliares
 
     public Parejas() {
         sw = 0;
-        t = new Tablero(6);
-        t.genAleatorio();
+        tablero = new Tablero(6);
+        tablero.genAleatorio();
         initComponents();
         configVentana();
     }
 
     public void configVentana() {
         setTitle("¡Voltea y Juega!");
-        setSize(380, 380);
+        setSize(1000, 1000);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     public void initComponents() {
-        int d = t.getDim();
+        int d = tablero.getDim();
         imagenes = new ImageIcon[d * d / 2 + 1];
         imagenes[0] = null;
-        for (int i = 1; i <= d * d / 2; i++)
-            imagenes[i] = new ImageIcon(getClass().getResource("./" + i + ".bmp"));
+        for (int i = 1; i <= d * d / 2; i++) {
+            Image img = new ImageIcon(getClass().getResource("./images/" + i + ".png")).getImage();
+            Image imageSize = img.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+            imagenes[i] = new ImageIcon(imageSize);
+        }
+
         JPanel A = new JPanel(new GridLayout(d, d));
         botones = new JButton[d][d];
         for (int i = 0; i < d; i++) {
             for (int j = 0; j < d; j++) {
                 botones[i][j] = new JButton();
-                botones[i][j].setBackground(Color.DARK_GRAY);
-
+                botones[i][j].setBackground(Color.white);
                 botones[i][j].addActionListener(this);
                 A.add(botones[i][j]);
             }
@@ -50,21 +53,21 @@ public class Parejas extends JFrame implements ActionListener {
     public void accion(int x, int y) {
         switch (sw) {
             case 0:
-                if (!t.esClic(x, y)) {
-                    t.clic(x, y);
-                    botones[x][y].setIcon(imagenes[t.getPos(x, y)]);
+                if (!tablero.esClic(x, y)) {
+                    tablero.clic(x, y);
+                    botones[x][y].setIcon(imagenes[tablero.getPos(x, y)]);
                     sw = 1;
                     a = x;
                     b = y;
                 }
                 break;
             case 1:
-                if (!t.esClic(x, y)) {
-                    t.clic(x, y);
-                    botones[x][y].setIcon(imagenes[t.getPos(x, y)]);
+                if (!tablero.esClic(x, y)) {
+                    tablero.clic(x, y);
+                    botones[x][y].setIcon(imagenes[tablero.getPos(x, y)]);
                     ii = x;
                     jj = y;
-                    if (t.getPos(a, b) != t.getPos(ii, jj))
+                    if (tablero.getPos(a, b) != tablero.getPos(ii, jj))
                         sw = 2;
                     else
                         sw = 0;
@@ -73,20 +76,20 @@ public class Parejas extends JFrame implements ActionListener {
             case 2:
                 botones[a][b].setIcon(null);
                 botones[ii][jj].setIcon(null);
-                t.clic(a, b);
-                t.clic(ii, jj);
+                tablero.clic(a, b);
+                tablero.clic(ii, jj);
                 sw = 0;
                 break;
         }
     }
 
     public void actionPerformed(ActionEvent ae) {
-        int d = t.getDim();
+        int d = tablero.getDim();
         for (int i = 0; i < d; i++) {
             for (int j = 0; j < d; j++) {
                 if (botones[i][j] == ae.getSource()) {
                     accion(i, j);
-                    if (t.esCompleto()) {
+                    if (tablero.esCompleto()) {
                         JOptionPane.showMessageDialog(this, "Felicidades", "¡has ganado!",
                                 JOptionPane.INFORMATION_MESSAGE, null);
                         System.exit(0);
---
+++ src/com/turn_play/concentrese/Principal.java
@@ -0,0 +1,12 @@
+package com.turn_play.concentrese;
+
+public class Principal {
+    public static void main(String[] a3d) {
+        ejecutar();
+
+    }
+
+    private static void ejecutar() {
+        Parejas pareja = new Parejas();
+    }
+}
